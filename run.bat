29c775b603e06a9117eed714e72b73e18c1fca04
CahBantul <fardan.nozami@gmail.com>
2022-09-26 00:11:29 -0700

    ad bat file

---
+++ run.bat
@@ -0,0 +1,22 @@
+    @echo off
+setlocal enabledelayedexpansion enableextensions
+
+tskill python
+
+set i=-1
+
+for %%f in (MATICUSDT,GMTUSDT,CHZUSDT,TRBUSDT,SANDUSDT) do (
+
+ set /a i=!i!+1
+ set coin[!i!]=%%f
+)
+set lastindex=!i!
+
+for /L %%f in (0,1,!lastindex!) do ( 
+  start /min python passivbot.py binance_01 !coin[%%f]! configs/live/!coin[%%f]!.json
+)
+
+
+
+
+exit
